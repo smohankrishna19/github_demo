09a354667b40cac277a5954f5e066f8a312aa3dc
Mohan Krishna <61688825+smohankrishna19@users.noreply.github.com>
2021-07-02 14:20:40 +0530

    Initial commit of Math.py

---
+++ Math.py
@@ -0,0 +1,12 @@
+# Add implementation
+def add(x,y):
+    return x+y;
+# Subtract implementation
+def subtract(x,y):
+    pass
+# Multiply implementation
+def multiply(x,y):
+    return x*y;
+# Divide implementation
+def divide(x,y):
+	pass
